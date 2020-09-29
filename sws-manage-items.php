64484008b928dcaabc667917e5b115c1f37eca7c
Sharon Stromberg <sharon@ccharacter.com>
2020-09-29 18:01:29 -0400

    logging

--- sws-manage-items.php
+++ sws-manage-items.php
@@ -53,7 +53,9 @@ function sws_manage_items_activate() {
     include_once( ABSPATH . '/wp-admin/includes/plugin.php' );
   }
   
-	if ( function_exists('acf_add_local_field_group') ) { sws_manage_items_create_acfgroup(); }
+	if ( current_user_can( 'activate_plugins' ) && function_exists('acf_add_local_field_group') ) { 
+		sws_manage_items_create_acfgroup(); 
+	} else { error_log("No ACF Field",true); }
   
   if ( current_user_can( 'activate_plugins' ) && ! class_exists( 'GFCommon' ) ) {
     // Deactivate the plugin.
