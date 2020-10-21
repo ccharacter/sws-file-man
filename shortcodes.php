0f8a4ee0da1cd6977a0f000f66cdd739519cae95
Sharon Stromberg <sharon@ccharacter.com>
2020-10-21 17:46:06 -0400

    separate shortcode for categories

--- shortcodes.php
+++ shortcodes.php
@@ -61,4 +61,27 @@ function sws_mg_items_display_func($atts) {
 add_shortcode('sws_display_items', 'sws_mg_items_display_func'); 
 
 
+// SHORTCODE FOR Displaying CATEGORIES
+function sws_mg_items_cat_func($atts) {
+	$a=shortcode_atts(array(
+	  'list_title' => 'View Posts by Category',
+	  'show_count' => 1,
+	  'hide_empty' => true,
+	  'depth' => 5,
+	  'show_option_all' => 'SEE ALL',
+	  'exclude' => "1",
+	  'limit' => 15
+	), $atts);
+	// NOTE TO SELF: SHORTCODE_ATTS DOESN'T LIKE UPPERCASE!!!!
+	
+	$mytext="<h3 class='c-block__heading-title u-theme--color--darker'>".$a['list_title']."</h3>";	
+	
+	$myCats=wp_list_categories( array('depth'=>$a['depth'],'hide_title_if_empty'=>$a['hide_empty'],'show_count'=>$a['show_count'],'echo'=>0,'exclude'=>$a['exclude'],'show_option_all'=>$a['show_option_all'],'title_li'=>''));
+	
+	return $mytext.$myCats;
+}
+
+// register shortcode
+add_shortcode('sws_display_categories', 'sws_mg_items_cat_func'); 
+
 ?>
